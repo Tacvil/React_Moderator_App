d0ae3d4f252b136072a15c0d5509375ce1aab966
Tacvil <83348678+Tacvil@users.noreply.github.com>
2024-03-16 15:56:52 +0700

    notify v1

--- src/Annontiment.js
+++ src/Annontiment.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { useItem } from './ItemContext'; // Импортируем контекст для item
 
+
+// Функция для получения токена доступа
+async function getAccessToken(serviceAccount) {
+  try {
+
+  } catch (error) {
+    console.error('Error getting access token:', error);
+    throw error;
+  }
+}
+
 const Annontiment = () => {
   const { item } = useItem(); // Получаем item из контекста
+  const [serviceAccount, setServiceAccount] = useState(null); // Состояние для хранения содержимого файла JSON
+  const [accessToken, setAccessToken] = useState(null); // Состояние для хранения токена доступа
+
+  // Функция для чтения файла JSON и получения токена доступа
+  const handleFileChange = async (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = async function(event) {
+        const content = event.target.result;
+        const parsedContent = JSON.parse(content);
+        setServiceAccount(parsedContent);
+        try {
+          const token = await getAccessToken(parsedContent);
+          setAccessToken(token);
+        } catch (error) {
+          console.error('Error getting access token:', error);
+        }
+      }
+      reader.readAsText(file);
+    }
+  };
 
   // Проверяем, есть ли данные в item
   if (!item) {
@@ -39,6 +72,23 @@ const Annontiment = () => {
       <div style={{ marginLeft: '20px' }}> {/* Отступ слева для размещения справа от слайдера */}
         <h3>{item.title}</h3>
         <p>{item.description}</p>
+        
+        {/* Поле для загрузки файла */}
+        <input type="file" onChange={handleFileChange} accept=".json" />
+
+        {/* Отображение содержимого файла и токена доступа */}
+        {serviceAccount && (
+          <div>
+            <h4>Service Account Key:</h4>
+            <pre>{JSON.stringify(serviceAccount, null, 2)}</pre>
+          </div>
+        )}
+        {accessToken && (
+          <div>
+            <h4>Access Token:</h4>
+            <pre>{accessToken}</pre>
+          </div>
+        )}
       </div>
     </div>
   );
